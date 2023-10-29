180d58983bf0376d70cc94d69542773df57a8953
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-10-29 11:24:06 +0530

    Remove unwanted 9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/components/Utility/Footer'
 import PreLoader from '@/components/Utility/PreLoader'
-import type { Metadata } from 'next'
 import './globals.css'
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-"use client"
 import About from "@/components/Home/About"
+import Experience from "@/components/Home/Experience"
 import LandingPage from "@/components/Home/LandingPage"
 import Projects from "@/components/Home/Projects"
 import Navbar from "@/components/Utility/Navbar"
-import { useState } from "react"
 
 
 export default function Home() {
-  const [first, setfirst] = useState(" ")
   return (
     <main >
       {/* <ParticleBackground/> */}
