b17ed0984aef5d73f5d738ae39132475679d09db
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-10-29 11:16:39 +0530

    Remove unwanted 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import Experience from "@/components/Home/Experience"
 import LandingPage from "@/components/Home/LandingPage"
 import Projects from "@/components/Home/Projects"
 import Navbar from "@/components/Utility/Navbar"
-import ParticleBackground from "@/components/Utility/ParticleBackground"
-import PreLoader from "@/components/Utility/PreLoader"
 import { useState } from "react"
 
 
