cb0ea395001265f47bfc807014f8ed2c1fc5d3fd
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-09-13 22:13:34 +0530

    Create projects page

--- src/app/myprojects/page.tsx
+++ src/app/myprojects/page.tsx
@@ -4,7 +4,7 @@ function Myprojects() {
   return (
     <section id="skills" className=' px-5  md:py-10   py-16 w-full' >
       <div className="container m-auto flex  md:flex-row flex-col gap-10 bg-grey-50 " >
-        <ShowProjects start={0} />
+        <ShowProjects start={0} end={99} />
       </div>
     </section>
   )
